2fa92ad862c28b06ba700e238c5c46630e29efd1
danu182 <danu182.182@gmail.com>
2024-09-19 16:50:03 +0700

    First Commit Crud Category

---
+++ resources/views/Back/category/index.blade.php
@@ -0,0 +1,82 @@
+@extends('Back.layout.template')
+
+@section('content')
+    <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h1 class="h2">Categories</h1>
+      </div>
+
+      <div class="mt-3">
+        <button class="btn btn-success mb-2" data-bs-toggle="modal" data-bs-target="#modalCreate">create</button>
+       
+        @if ($errors->any())
+        <div class="mt-3">
+                <div class="alert alert-danger">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        @endif
+
+        @if(session('success'))
+            <div class="mt-3">
+                <div class="alert alert-success">
+                        {{ session('success') }}
+                </div>
+            </div>
+        @endif
+       
+        <table class="table table-striped table-hover">
+            <thead>
+                <th>no</th>
+                <th>nama</th>
+                <th>slug</th>
+                <th>created_at</th>
+                <th>function</th>
+            </thead>
+            <tbody>
+                @forelse  ($category as $item)
+                    <tr>
+                        <td>{{ $item->id }}</td>
+                        <td>{{ $item->name }}</td>
+                        <td>{{ $item->slug }}</td>
+                        <td>{{ $item->created_at }}</td>
+                        <td>
+                            <div class="text-center">
+                                <button class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#modalUpdate{{ $item->id }}">edit</button>
+                                <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete{{ $item->id }}">hapus</button>
+                            </div>
+                        </td>
+                    </tr>                    
+                @empty
+                    <tr><td colspan="5">tidak ada isi</td></tr>
+                @endforelse
+            </tbody>
+        </table>
+      </div>
+
+      {{-- modal  start --}}
+      
+      {{-- modal create start --}}
+      @include('Back.category.create-modal')
+      {{-- modal create end --}}
+      
+      
+      {{-- modal update start --}}
+      @include('Back.category.update-modal')
+      {{-- modal update end --}}
+      
+      
+      {{-- modal delete start --}}
+      @include('Back.category.delete-modal')
+      {{-- modal delete end --}}
+      
+      
+      
+      {{-- modal end --}}
+      
+    </main>
+@endsection
---
+++ resources/views/Back/layout/template.blade.php
@@ -0,0 +1,70 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Dashboard Template · Bootstrap v5.0</title>
+
+     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  
+    <link rel="stylesheet" href="{{ asset('back/css/custom.css') }}">
+
+
+    
+
+    <!-- Bootstrap core CSS -->
+{{-- <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"> --}}
+
+{{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+
+    
+    
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('back/css/dashboard.css') }}" rel="stylesheet">
+  </head>
+  <body>
+
+{{-- header start --}}
+    @include('Back.layout.header')
+{{-- header end --}}
+
+<div class="container-fluid">
+  <div class="row">
+
+    {{-- sidebar start  --}}
+        @include('Back.layout.sidebar')
+    {{-- sidebar start  --}}
+    
+
+    {{-- start main content --}}
+        @yield('content')
+    {{-- end main content --}}
+
+
+
+
+  </div>
+</div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  
+      <script src="https://cdn.jsdelivr.net/npm/feather-icons@4.28.0/dist/feather.min.js" integrity="sha384-uO3SXW5IuS1ZpFPKugNNWqTZRRglnUJK6UAZ/gxOX80nxEkN9NcGZTftn6RzhGWE" crossorigin="anonymous"></script><script src="https://cdn.jsdelivr.net/npm/chart.js@2.9.4/dist/Chart.min.js" integrity="sha384-zNy6FEbO50N+Cg5wap8IKA4M/ZnLJgzc6w2NqACZaK0u0FXfOWRRJOnQtpZun8ha" crossorigin="anonymous"></script><script src="{{ asset('back/js/dashoard.js') }}"></script>
+  </body>
+</html>
